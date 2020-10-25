d78289afa7ab6679d0a36590d8c65a19761b89a1
Morgue16 <45204675+Morgue16@users.noreply.github.com>
2020-10-24 22:52:22 -0400

    Create index.js

---
+++ Portfolio 1/src/components/Example/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Example() {
+    return (
+        <div>
+            <div class = "header">
+                <h1>Moble Development Portfolio 1</h1>
+            </div>
+                
+            <div>
+                <p>Hi, my name is Morgan Prenitzer.</p>
+                <p>I have been struggeling to remember when all my assignments are due.</p>
+                <p>I will try not to miss more in the future.</p>
+            </div>
+            <div class = "footer">
+                <h2>Moble Development Portfolio 1</h2>
+            </div>
+        </div>   
+    );
+}
+
+export default Example
